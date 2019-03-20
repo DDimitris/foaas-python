906baffb65121efbe2f3c6849a7d78c357654a77
Dimitris Dedousis <dimitris.dedousis@gmail.com>
2019-03-21 00:07:11 +0200

    Introducing major changes
    Metaprogramming was used in order to dynamically create methods and
    remove the hardcoded actions and urls. A call is made to
    https://foaas.com/operations in order to get all the operations as well
    as their paths. Using metaprogramming, this library is always up-to-date
    regardless of any possible change that is made to the actions on the
    foaas framework.
    
    The setFuckingAddress() method was added in order for the user to be
    able to declare a custom address in case he has a personal setup of
    foaas framework. The default address that is used is https://foaas.com
    because it supports both IPv4 and IPv6.
    
    The FuckingOperations() class is created in order to get and manipulate
    all the results from the /operations path provided by foaas.
    
    The CLI support was removed in order for a user to use this library
    within his code and not as a standalone library.

--- foaas.py
+++ foaas.py
@@ -1,11 +1,6 @@
 import random
 import requests
-from optparse import OptionParser
-
-try:
-    from urllib import quote
-except ImportError:
-    from urllib.parse import quote
+import json
 
 __AUTHOR__ = 'Derek Payton <derek.payton@gmail.com>'
 __LICENSE__ = 'MIT'
@@ -13,11 +8,9 @@ __VERSION__ = '0.2.0'
 
 
 class FuckingResponse(object):
-    ''' Takes care of making the actual request in the specified format. '''
-    def __init__(self, url, language=None):
+
+    def __init__(self, url):
         self.url = url
-        self.language = language
-        self.is_secure = url.startswith('https')
         self._html = None
         self._json = None
         self._text = None
@@ -25,7 +18,7 @@ class FuckingResponse(object):
     def make_request(self, accept):
         return requests.get(self.url, headers={
             'Accept': accept,
-            'Accept-Language': self.language or 'en-us'
+            'Accept-Language': 'en-us'
         })
 
     @property
@@ -47,117 +40,75 @@ class FuckingResponse(object):
         return self._html.text
 
 
+class FuckingOperations(object):
+
+    def __init__(self, url):
+        self.operations_dict = json.loads(
+            FuckingResponse(url + "operations").text)
+
+    def list_of_urls(self):
+        urls = list(map(lambda url_value: url_value['url'],
+                        self.operations_dict))
+        return urls
+
+    def list_of_actions(self):
+        actions = list(map(lambda url: url.split('/')[1], list_of_urls()))
+        return actions
+
+    def dict_of_actions_urls(self):
+        actions_dict = dict(map(lambda item: (item['url'].split(
+            '/')[1], item['url']), self.operations_dict))
+        return actions_dict
+
+
 class Fuck(object):
-    actions = {
-        'awesome': 'awesome/{from}',
-        'ballmer': 'ballmer/{name}/{company}/{from}',
-        'because': 'because/{from}',
-        'bus': 'bus/{name}/{from}',
-        'bye': 'bye/{from}',
-        'caniuse': 'caniuse/{name}/{from}',
-        'chainsaw': 'chainsaw/{name}/{from}',
-        'cool': 'cool/{from}',
-        'diabetes': 'diabetes/{from}',
-        'donut': 'donut/{name}/{from}',
-        'everyone': 'everyone/{from}',
-        'everything': 'everything/{from}',
-        'fascinating': 'fascinating/{from}',
-        'field': 'field/{name}/{from}/{reference}',
-        'flying': 'flying/{from}',
-        'king': 'king/{name}/{from}',
-        'life': 'life/{from}',
-        'linus': 'linus/{name}/{from}',
-        'madison': 'madison/{name}/{from}',
-        'nugget': 'nugget/{name}/{from}',
-        'off': 'off/{name}/{from}',
-        'outside': 'outside/{name}/{from}',
-        'pink': 'pink/{from}',
-        'thanks': 'thanks/{from}',
-        'that': 'that/{from}',
-        'thing': '{thing}/{from}',
-        'this': 'this/{from}',
-        'shakespeare': 'shakespeare/{name}/{from}',
-        'what': 'what/{from}',
-        'xmas': 'xmas/{name}/{from}',
-        'yoda': 'yoda/{name}/{from}',
-        'you': 'you/{name}/{from}',
-    }
 
-    def __init__(self, secure=False, language=None):
-        self.secure = secure
-        self.language = language
+    GLOBAL_URL = "https://foaas.com/"
 
-    def __getattr__(self, attr):
-        path = self.actions.get(attr)
-        if path is None:
-            raise AttributeError
+    def __init__(self):
+        self.initialized = False
 
-        def outer(path):
-            def inner(secure=False, **kwargs):
-                url = self.build_url(path, **kwargs)
-                return FuckingResponse(url, language=self.language)
-            return inner
+    def __getattr__(self, method_name):
+        if not self.initialized:
+            self.initialized = True
+            self.foperations = FuckingOperations(self.GLOBAL_URL)
+            for action, url in self.foperations.dict_of_actions_urls().items():
+                self.add_meta_method(action, url)
+            return getattr(self, method_name)
+        else:
+            raise AttributeError(
+                "The method you are trying to call does not exists!")
 
-        return outer(self.actions[attr])
+    def add_meta_method(self, action, url):
+
+        def method(self, **kwargs):
+            final_url = self.build_url(url, **kwargs)
+            return FuckingResponse(final_url)
+
+        setattr(Fuck, action, method)
+
+    def build_url(self, url, **kwargs):
+        url_split = url.split('/')
+        final_url = self.GLOBAL_URL + url_split[1] + "/"
+        for i in range(2, len(url_split)):
+            final_url += kwargs[url_split[i].replace(":", "") + "_"] + "/"
+        return final_url[:-1]
 
     def random(self, **kwargs):
-        applicable_actions = []
-        for action in self.actions.keys():
-            uri = self.actions[action]
-            if uri.count('{') != len(kwargs):
-                continue
-            for param in kwargs:
-                if '{{{0}}}'.format(param.rstrip('_')) not in uri:
-                    break
-            else:
-                applicable_actions.append(action)
+        actions = []
+        urls_list = self.foperations.list_of_urls()
+        string_of_names = ""
+        for key, value in kwargs.items():
+            string_of_names += key
+        for url in urls_list:
+            url_split = url.replace(":", "").split('/')[2:]
+            if len(kwargs.items()) == len(url_split):
+                if all(x in string_of_names for x in url_split):
+                    actions.append(url)
+        return FuckingResponse(self.build_url(random.choice(actions),
+                                              **kwargs))
 
-        choice = getattr(self, random.choice(applicable_actions))
-        return choice(**kwargs)
-
-    def build_url(self, path, **kwargs):
-        # use from_ since from is a keyword. *grumble*
-        params = dict([(k.rstrip('_'), quote(v)) for k, v
-            in kwargs.items() if v])
-        url = '{protocol}://foaas.herokuapp.com/{path}'.format(
-            protocol='https' if self.secure else 'http',
-            path=path.format(**params))
-        return url
-
+    def setFuckingAddress(self, address):
+        self.GLOBAL_URL = address
 
 fuck = Fuck()
-
-if __name__ == '__main__':
-    parser = OptionParser()
-
-    parser.add_option('-a', '--action', dest='action', default='off',
-        help='Name of the action to perform')
-
-    parser.add_option('-n', '--name', dest='name',
-        help='Who do you want to fuck off?')
-
-    parser.add_option('-f', '--from', dest='from', help='Who are you?')
-
-    parser.add_option('-c', '--company', dest='company',
-        help='Which company do you want to fuck off?')
-
-    parser.add_option('-t', '--thing', dest='thing',
-        help='What thing do you want to fuck off?')
-
-    parser.add_option('-r', '--reference', dest='reference',
-        help='Who do you want to reference?')
-
-    parser.add_option('-u', '--url', action='store_true', dest='url',
-        help='Only display the URL (useful for c/ping)')
-
-    (options, args) = parser.parse_args()
-
-    options = vars(options)
-    action = options.pop('action')
-    url_only = options.pop('url')
-
-    fucking = getattr(fuck, action)(**options)
-    if url_only:
-        print(fucking.url)
-    else:
-        print(fucking.text)
